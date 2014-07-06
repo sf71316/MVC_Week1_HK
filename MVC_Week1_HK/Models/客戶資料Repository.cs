4af4c8317b4181f6d05b221b6d5b32de8b860432
sf71316 <sf71316@gmail.com>
2014-07-06 20:08:19 +0800

    建置Repository

--- MVC_Week1_HK/Models/客戶資料Repository.cs
+++ MVC_Week1_HK/Models/客戶資料Repository.cs
@@ -6,7 +6,19 @@ namespace MVC_Week1_HK.Models
 {   
 	public  class 客戶資料Repository : EFRepository<客戶資料>, I客戶資料Repository
 	{
-
+        public override IQueryable<客戶資料> All()
+        {
+            return base.All().Where(p=>p.IsDelete==false);
+        }
+        public 客戶資料 Find(Func<客戶資料, bool> func)
+        {
+            return base.All().Where(p => p.IsDelete == false).FirstOrDefault(func);
+        }
+        public override void Delete(客戶資料 entity)
+        {
+            entity.IsDelete = true;
+            this.UnitOfWork.Commit();
+        }
 	}
 
 	public  interface I客戶資料Repository : IRepository<客戶資料>
--- MVC_Week1_HK/Models/客戶銀行資訊Repository.cs
+++ MVC_Week1_HK/Models/客戶銀行資訊Repository.cs
@@ -6,7 +6,19 @@ namespace MVC_Week1_HK.Models
 {   
 	public  class 客戶銀行資訊Repository : EFRepository<客戶銀行資訊>, I客戶銀行資訊Repository
 	{
-
+        public override IQueryable<客戶銀行資訊> All()
+        {
+            return base.All().Where(p => p.IsDelete == false);
+        }
+        public 客戶銀行資訊 Find(Func<客戶銀行資訊, bool> func)
+        {
+            return base.All().Where(p => p.IsDelete == false).FirstOrDefault(func);
+        }
+        public override void Delete(客戶銀行資訊 entity)
+        {
+            entity.IsDelete = true;
+            this.UnitOfWork.Commit();
+        }
 	}
 
 	public  interface I客戶銀行資訊Repository : IRepository<客戶銀行資訊>
